ece4cd8e9597df99431369f278b05dd9b15499be
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-05-09 19:24:52 -0300

    chore(client.js): remove unused code

--- public/client.js
+++ public/client.js
@@ -71,21 +71,21 @@ function back_video(){
    location.reload();
 }
 
-function hexToRgb(hex) {
-   // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-   var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-
-   hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-      return r + r + g + g + b + b;
-   });
-
-   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-   return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-   } : null;
-}
+// function hexToRgb(hex) {
+//    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+//    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+//
+//    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+//       return r + r + g + g + b + b;
+//    });
+//
+//    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+//    return result ? {
+//       r: parseInt(result[1], 16),
+//       g: parseInt(result[2], 16),
+//       b: parseInt(result[3], 16)
+//    } : null;
+// }
 
 function main_interval(){
 mainInterval = setInterval(function(){
@@ -276,18 +276,19 @@ $(document).ready(function() {
 		$("#flip_video_div").hide();
 	}
 
-   $("#canvas_them").hammer().on("doubletap", function(){
-      if($("#img_canvas").is(":visible")){
-         $("#remove").click();
-         stopDrawing = true;
-         $("#canvas_them").css("z-index", "0");
-      }
-   });
 
-   $("#img_canvas").hammer().on("doubletap", function(){
-      stopDrawing = false;
-      $("#canvas_them").css("z-index", "1");
-   });
+   // $("#canvas_them").hammer().on("doubletap", function(){
+   //    if($("#img_canvas").is(":visible")){
+   //       $("#remove").click();
+   //       stopDrawing = true;
+   //       $("#canvas_them").css("z-index", "0");
+   //    }
+   // });
+   //
+   // $("#img_canvas").hammer().on("doubletap", function(){
+   //    stopDrawing = false;
+   //    $("#canvas_them").css("z-index", "1");
+   // });
 
 	//window.location.hostname does not work with "localhost"
 	socket = io.connect("http://" + window.location.hostname + ":8081");
